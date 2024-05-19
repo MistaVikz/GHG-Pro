5ff5284d26128597716abb07d70e7dd4d3c8075a
Mark Vickers <markdvickers@icloud.com>
2024-05-19 15:44:47 -0600

    Fixed offered volume bug and updated valid_project_data fail message

--- project_risk.py
+++ project_risk.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser()
         parser.add_argument('-i', '--input', choices=['excel', 'csv'], default='excel', help='Input file format')
+        parser.add_argument('-d', '--display', type=str, choices=['on', 'off'], default='on', help='Display console output')
         args = parser.parse_args()
         input_choice = args.input
+        display_output = args.display == 'on'
 
         risk_bucket_count, risk_factor_count, df_project, df_default_rates, df_recovery_potential = load_and_process_data(input_choice)
 
@@ -21,15 +23,14 @@ if __name__ == "__main__":
 
             # Calculate Risk Bucket Risk Scores and Ratings for each risk bucket 
             df_project = calculate_risk_bucket_scores(df_project,risk_bucket_count,risk_factor_count) 
-
             for i in range(1, risk_bucket_count + 1):
                 df_project[f'risk_bucket_{i}_rating'] = score_to_rating_vectorized(df_project[f'risk_bucket_{i}_score'])
-            
+    
             # Prepare for the simulation
             df_project = calculate_yearly_exposure(df_project, df_default_rates, df_recovery_potential, risk_bucket_count)
             df_project = calculate_yearly_expected_value(df_project, risk_bucket_count)
             df_project = calculate_yearly_standard_deviation(df_project,risk_bucket_count)
-        
+
             # Run the yearly simulations for all projects
             df_project = run_simulation(df_project, risk_bucket_count)
             overall_expected_value_percentage = df_project[[f'project_expected_value_percentage_year_{year}' for year in range(1, NUM_YEARS+1)]].mean(axis=1, skipna=True) * 10
@@ -43,15 +44,19 @@ if __name__ == "__main__":
             technology_table = create_group_table(df_project, 'technology')
             counterparty_table = create_group_table(df_project, 'counterparty')
             total_volumes_per_year = calculate_total_volumes_by_year(df_project)
-            
+    
             # Display and Export Data
-            display_project_risk_output(df_counts, top_projects, bottom_projects, country_table, technology_table, counterparty_table, total_volumes_per_year)
+            if display_output:
+                display_project_risk_output(df_counts, top_projects, bottom_projects, country_table, technology_table, counterparty_table, total_volumes_per_year)
             output_folder = create_output_folder()
             export_project_risk_output(output_folder, top_projects, bottom_projects, country_table, technology_table, counterparty_table, df_counts, total_volumes_per_year)
             export_ghg_data(output_folder, df_project,df_default_rates,df_recovery_potential)
         else:
-            print("Data not loaded properly from GHG_Data.xlsx")
-
+            if input_choice == 'excel':
+                print("Data not loaded properly from GHG_Data.xlsx")
+            else:
+                print("Data not loaded properly from GHG_Data.csv, Default_Rates.csv, and/or Recovery_Potential.csv")
+                
     except FileNotFoundError as e:
         logging.error(f"File not found: {str(e)}")
     except pd.errors.EmptyDataError as e:
